86447fd0c0ef1b315e4f6370b0f313c66afbdbda
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-11-24 16:53:55 +0100

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer.jsx";
-import Header from "./components/Header/Header.jsx"
-import Main from "./components/Main/Main.jsx";
-
+import Header from "./components/Header/Header.jsx";
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Main />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/datenschutz" element={<h1>Datenschutz TEST</h1>} />
+        </Routes>
       <Footer />
     </div>
   );
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,32 @@
-export default function LandingPage() {
+import React from "react";
+import "./LandingPage.css";
+import hero1 from "../../assets/hero1.png";
+
+function LandingPage() {
   return (
-    <div>
-      <h1>test Landing Page</h1>
+    <div className="main-hero">
+      <main className="flex items-center justify-between px-10 py-20">
+        <div className="w-1/2 pr-10">
+          <h1 className="text-6xl font-extrabold text-gray-900 mb-4">
+            Invest smart. Learn easy.
+          </h1>
+          <p className="mt-4 text-gray-600 text-lg leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mt-6 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            JETZT LOSLEGEN
+          </button>
+        </div>
+        <img src={hero1} className="w-1/2" alt="Hero" />
+      </main>
     </div>
   );
 }
+
+export default LandingPage;
