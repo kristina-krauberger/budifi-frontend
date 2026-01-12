e5699a75d7ad43305e4c5f3b589298ea06e321b7
Kristina Krauberger <krauberger.kristina@gmail.com>
2026-01-12 11:59:40 +0100

    feat: add video loading placeholder container.

--- src/components/LessonVideo/LessonVideo.jsx
+++ src/components/LessonVideo/LessonVideo.jsx
@@ -6,14 +6,11 @@ import { storage } from "../../firebase"; // Pfad anpassen je nach Struktur
 function LessonVideo({ course, coursesData, setCourse }) {
   const { isVideoCompleted, setIsVideoCompleted } = useOutletContext();
   console.log("LESSON VIDEO📌", typeof isVideoCompleted, isVideoCompleted);
-  const {courseId, lessonId} = useParams();
+  const { courseId, lessonId } = useParams();
   const [videoUrl, setVideoUrl] = useState(null);
 
   useEffect(() => {
-    const videoRef = ref(
-      storage,
-      `course${courseId}_lesson${lessonId}.mp4`
-    );
+    const videoRef = ref(storage, `course${courseId}_lesson${lessonId}.mp4`);
     getDownloadURL(videoRef)
       .then((url) => {
         setVideoUrl(url);
@@ -25,21 +22,25 @@ function LessonVideo({ course, coursesData, setCourse }) {
 
   return (
     <div className="main-content">
-      {!videoUrl ? (
-        <p className="text-center">Loading Video...</p>
-      ) : (
-        <video
-          controls
-          className="w-full max-w-3xl mx-auto rounded-md shadow-md"
-        >
-          <source src={videoUrl} type="video/mp4" />
-        </video>
-      )}
+      <div className="w-[640px] h-[360px] bg-gray-200 flex bg-gray-200 flex items-center justify-center rounded-md shadow-md overflow-hidden">
+        {!videoUrl ? (
+          <div className="w-full h-full flex flex-col items-center justify-center">
+            <span className="text-4xl text-gray-500">▶</span>
+            <p className="text-center mt-2 text-gray-600">Loading Video...</p>
+          </div>
+        ) : (
+          <video controls className="w-full h-full object-cover rounded-md">
+            <source src={videoUrl} type="video/mp4" />
+          </video>
+        )}
+      </div>
+
       <button
         onClick={() => setIsVideoCompleted(!isVideoCompleted)}
-        className="border bg-amber-400 hover:"
+        className="border mt-10
+         bg-amber-400 hover:"
       >
-        LessonVideo
+        Click wenn Video abgeschlossen
       </button>
     </div>
   );
