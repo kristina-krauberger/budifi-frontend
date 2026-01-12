c97586c62f7301004ee5d73e7c2fe902bddcfd99
Kristina Krauberger <krauberger.kristina@gmail.com>
2026-01-12 17:17:14 +0100

    feat: highlight active button in Lesson Navbar via URL path.

--- src/components/LessonNavbar/LessonNavbar.jsx
+++ src/components/LessonNavbar/LessonNavbar.jsx
@@ -1,16 +1,46 @@
 import React from "react";
 import "../../App.css";
-import { Link } from "react-router";
-
+import { Link, useLocation } from "react-router";
 
 function LessonNavbar() {
+  const location = useLocation();
+  console.log("NAVBAR LOCATION:", location.pathname);
+
   return (
     <nav className="py-3">
       <div className="bg-neutral-100 border border-gray-300 rounded-lg shadow px-4 py-3">
         <ul className="flex justify-between gap-2 text-sm">
-          <Link to="video" className="w-full text-center px-3 py-2 border border-gray-200 bg-white rounded-md text-gray-700;">Video</Link>
-          <Link to="quiz" className="w-full text-center px-3 py-2 border border-gray-200 bg-white rounded-md text-gray-700;">Quiz</Link>
-          <Link to="summary" className="w-full text-center px-3 py-2 border border-gray-200 bg-white rounded-md text-gray-700;">Fazit</Link>
+          <Link
+            to="video"
+            className={`w-full text-center px-3 py-2 border rounded-md bg-white text-gray-700 transition ${
+              location.pathname.includes("video")
+                ? " border-grey-300"
+                : " border-gray-200 hover:border-gray-400"
+            }`}
+          >
+            Video
+          </Link>
+          <Link
+            to="quiz"
+            className={`w-full text-center px-3 py-2 border bg-white rounded-md text-gray-700 transition ${
+              location.pathname.includes("quiz")
+                ? " border-grey-300"
+                : " border-gray-200 hover:border-gray-400"
+            }
+          `}
+          >
+            Quiz
+          </Link>
+          <Link
+            to="summary"
+            className={`w-full text-center px-3 py-2 border bg-white rounded-md text-gray-700 transition ${
+              location.pathname.includes("summary")
+                ? " border-grey-300"
+                : " border-gray-200 hover:border-gray-400"
+            }`}
+          >
+            Fazit
+          </Link>
         </ul>
       </div>
     </nav>
